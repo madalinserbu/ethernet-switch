0b4992b9463673e668ea35011325f00695d39d0b
Serbu Ovidiu-Madalin <92184535+madalinserbu@users.noreply.github.com>
2024-11-01 01:34:13 +0200

    Add files via upload

--- switch.py
+++ switch.py
@@ -4,22 +4,16 @@ import struct
 import wrapper
 import threading
 import time
+import os
 from wrapper import recv_from_any_link, send_to_link, get_switch_mac, get_interface_name
-
-# Tabel de forwarding pentru învățarea adreselor MAC
-forwarding_table = {}
-vlan_table = {}  # Tabel pentru VLAN-uri, unde cheia este VLAN ID și valoarea o listă de interfețe
-
-# Timeout pentru adresele MAC în secunde
-MAC_TIMEOUT = 300
 
 def parse_ethernet_header(data):
     dest_mac = data[0:6]
     src_mac = data[6:12]
     ether_type = (data[12] << 8) + data[13]
-
     vlan_id = -1
-    if ether_type == 0x8100:
+
+    if ether_type == 0x8200:
         vlan_tci = int.from_bytes(data[14:16], byteorder='big')
         vlan_id = vlan_tci & 0x0FFF
         ether_type = (data[16] << 8) + data[17]
@@ -27,62 +21,111 @@ def parse_ethernet_header(data):
     return dest_mac, src_mac, ether_type, vlan_id
 
 def create_vlan_tag(vlan_id):
-    return struct.pack('!H', 0x8100) + struct.pack('!H', vlan_id & 0x0FFF)
+    return struct.pack('!H', 0x8200) + struct.pack('!H', vlan_id & 0x0FFF)
 
-def send_bdpu_every_sec():
-    while True:
-        bpdu_data = b'\x01\x80\xc2\x00\x00\x00' + get_switch_mac()  # Exemplu de BPDU
-        for i in range(num_interfaces):
-            send_to_link(i, len(bpdu_data), bpdu_data)
-        time.sleep(1)
+def is_unicast(mac):
+    return (mac[0] & 1) == 0
+
+def load_configuration(switch_id):
+    config_path = f'configs/switch{switch_id}.cfg'
+    vlan_map = {}
+    trunk_ports = set()
+
+    try:
+        with open(config_path, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if line and not line.startswith("#"):
+                    parts = line.split()
+                    if len(parts) == 2:
+                        interface, vlan_or_trunk = parts
+                        if vlan_or_trunk == 'T':
+                            trunk_ports.add(interface)
+                        else:
+                            vlan_map[interface] = int(vlan_or_trunk)
+        print(f"[INFO] Loaded configuration for switch {switch_id}")
+    except FileNotFoundError:
+        print(f"[ERROR] Configuration file {config_path} not found.")
+    except ValueError:
+        print(f"[ERROR] Invalid configuration format in {config_path}")
+
+    return vlan_map, trunk_ports
 
 def main():
     switch_id = sys.argv[1]
+    config_file = f'configs/switch{switch_id}.cfg'
     num_interfaces = wrapper.init(sys.argv[2:])
-    interfaces = range(num_interfaces)
-    print("# Starting switch with id {}".format(switch_id), flush=True)
-    print("[INFO] Switch MAC:", ':'.join(f'{b:02x}' for b in get_switch_mac()))
+    interfaces = range(0, num_interfaces)
 
-    # Inițiere fir pentru trimitere BPDU
-    threading.Thread(target=send_bdpu_every_sec, daemon=True).start()
+    print("# Starting switch with id {}".format(switch_id), flush=True)
+    print("[INFO] Switch MAC", ':'.join(f'{b:02x}' for b in get_switch_mac()))
+
+    vlan_map, trunk_ports = load_configuration(switch_id)
+    mac_table = {}
 
     while True:
         interface, data, length = recv_from_any_link()
-
         dest_mac, src_mac, ethertype, vlan_id = parse_ethernet_header(data)
 
-        # MAC learning
-        forwarding_table[src_mac] = (interface, time.time())
+        interface_name = get_interface_name(interface)
+        if interface_name in vlan_map:
+            port_vlan_id = vlan_map[interface_name]
+            if vlan_id == -1:
+                vlan_id = port_vlan_id
+                data = data[:12] + create_vlan_tag(vlan_id) + data[12:]
+                length += 4
+            elif vlan_id != port_vlan_id:
+                continue
+        elif interface_name in trunk_ports:
+            port_vlan_id = vlan_id
 
-        # Ștergerea adreselor MAC expirate
-        for mac in list(forwarding_table.keys()):
-            if time.time() - forwarding_table[mac][1] > MAC_TIMEOUT:
-                del forwarding_table[mac]
+        dest_mac = ':'.join(f'{b:02x}' for b in dest_mac)
+        src_mac = ':'.join(f'{b:02x}' for b in src_mac)
+        print(f'Destination MAC: {dest_mac}')
+        print(f'Source MAC: {src_mac}')
+        print(f'EtherType: {ethertype}')
+        print(f'VLAN ID: {vlan_id}')
+        print("Received frame of size {} on interface {}".format(length, interface), flush=True)
 
-        # Conversia adreselor MAC la format uman
-        dest_mac_str = ':'.join(f'{b:02x}' for b in dest_mac)
-        src_mac_str = ':'.join(f'{b:02x}' for b in src_mac)
-        print(f'Destination MAC: {dest_mac_str}, Source MAC: {src_mac_str}, EtherType: {ethertype}, VLAN ID: {vlan_id}')
+        mac_table[(src_mac, vlan_id)] = interface
 
-        # VLAN support: Rutează cadrele în funcție de VLAN ID
-        if vlan_id != -1:
-            if vlan_id not in vlan_table:
-                vlan_table[vlan_id] = []
-            vlan_table[vlan_id].append(interface)
-            # Rutează cadrul pe interfețele din același VLAN
-            for i in vlan_table[vlan_id]:
-                if i != interface:
-                    send_to_link(i, length, data)
-        else:
-            # Forwarding simplu pe baza MAC
-            if dest_mac in forwarding_table:
-                out_interface, _ = forwarding_table[dest_mac]
-                send_to_link(out_interface, length, data)
+        if is_unicast(data[0:6]):
+            if (dest_mac, vlan_id) in mac_table:
+                out_port = mac_table[(dest_mac, vlan_id)]
+                out_interface_name = get_interface_name(out_port)
+                if out_interface_name in vlan_map:
+                    if vlan_id == vlan_map[out_interface_name]:
+                        data = data[:12] + data[16:]
+                        length -= 4
+                send_to_link(out_port, length, data)
             else:
-                # Flooding dacă MAC-ul nu este cunoscut
-                for i in interfaces:
-                    if i != interface:
-                        send_to_link(i, length, data)
+                for port in interfaces:
+                    if port != interface:
+                        out_interface_name = get_interface_name(port)
+                        if out_interface_name in trunk_ports or (
+                            out_interface_name in vlan_map and vlan_map[out_interface_name] == vlan_id
+                        ):
+                            if out_interface_name in vlan_map:
+                                data_to_send = data[:12] + data[16:] if vlan_id == vlan_map[out_interface_name] else data
+                                adjusted_length = length - 4 if vlan_id == vlan_map[out_interface_name] else length
+                            else:
+                                data_to_send = data
+                                adjusted_length = length
+                            send_to_link(port, adjusted_length, data_to_send)
+        else:
+            for port in interfaces:
+                if port != interface:
+                    out_interface_name = get_interface_name(port)
+                    if out_interface_name in trunk_ports or (
+                        out_interface_name in vlan_map and vlan_map[out_interface_name] == vlan_id
+                    ):
+                        if out_interface_name in vlan_map:
+                            data_to_send = data[:12] + data[16:] if vlan_id == vlan_map[out_interface_name] else data
+                            adjusted_length = length - 4 if vlan_id == vlan_map[out_interface_name] else length
+                        else:
+                            data_to_send = data
+                            adjusted_length = length
+                        send_to_link(port, adjusted_length, data_to_send)
 
 if __name__ == "__main__":
     main()
